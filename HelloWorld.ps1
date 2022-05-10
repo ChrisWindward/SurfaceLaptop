6d2ef9e38a2cb015fe164f45c5690dca894cb947
ChrisWindward <101744397+ChrisWindward@users.noreply.github.com>
2022-05-10 11:20:47 -0700

    Update HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,1 +1,4 @@
-﻿Write-Host "Hello World"
+Write-Host " "
+Write-Host "Here are the top 10 CPU consuming processes right now"
+Write-Host " "
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
