84638ed2b62a1529f18333cef98e13b80448c67b
ChrisWindward <101744397+ChrisWindward@users.noreply.github.com>
2022-05-11 15:38:29 -0700

    Update RemoveSoftware.ps1

--- RemoveSoftware.ps1
+++ RemoveSoftware.ps1
@@ -3,16 +3,12 @@ Write-Host "Uninstalling default apps" -foregroundcolor Red
 $apps = @(
     # default Windows 10 apps
     "Microsoft.ZuneVideo"
-    "Microsoft.ZuneMusic"
-    "Microsoft.people"
-    "Microsoft.MicrosoftSolitaireCollection"
-    "Microsoft.MixedReality.Portal"
     )
 
 $appxprovisionedpackage = Get-AppxProvisionedPackage -Online
 
 foreach ($app in $apps) {
-    Write-Output 'Trying to remove $app'
+    Write-Output 'Trying to remove' $app
 
     Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
 
