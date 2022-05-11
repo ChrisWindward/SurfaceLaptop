2591f7cc72a015445bea17e3a51221c6e9f9d611
ChrisWindward <101744397+ChrisWindward@users.noreply.github.com>
2022-05-11 15:12:58 -0700

    Update RemoveSoftware.ps1

--- RemoveSoftware.ps1
+++ RemoveSoftware.ps1
@@ -9,15 +9,15 @@ $apps = @(
 
 $appxprovisionedpackage = Get-AppxProvisionedPackage -Online
 
-if ($app in $apps) {
-    Write-Output "Trying to remove $app"
+if ($app -contains $apps) {
+    Write-Output 'Trying to remove $app'
 
     Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
 
     ($appxprovisionedpackage).Where( {$_.DisplayName -EQ $app}) |
         Remove-AppxProvisionedPackage -Online
-    Write-Output "Uninstalled " $app
+    Write-Output 'Uninstalled ' $app
 }
 else {
-    Write-Output $app "was not found.
+    Write-Output $app 'was not found.'
 }
